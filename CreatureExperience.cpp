583fe230c8ae39f33f63e4953bc66af86ea62020
SamuelJoly23 <98899617+SamuelJoly23@users.noreply.github.com>
2022-02-20 09:43:33 -0500

    Update CreatureExperience.cpp

--- CreatureExperience.cpp
+++ CreatureExperience.cpp
@@ -1,34 +1,32 @@
 #include "CreatureExperience.h"
 
-// todo:
+// todo: DONE
 // Faire l'initialisation de tout les attributs
-CreatureExperience::CreatureExperience(const string& nom)
+CreatureExperience::CreatureExperience(const string& nom):Creature(nom)
 {
-	
+    //nom_ = nom; 
+    //this.nom_ = nom; proposition chargee td
 }
+//implementation apres ":"
+
+
+// todo: DONE VERIFIED
+// Faire l'initialisation de tout les attributs
+CreatureExperience::CreatureExperience(const string& nom, unsigned int attaque, unsigned int defense, unsigned int pointDeVie, unsigned int energie, Pouvoir pouvoir, unsigned int forceExp):Creature(nom, attaque, defense, pointDeVie, energie, pouvoir), forceExperience_(forceExp){}
+//implementation apres ":"
+
+// todo: DONE
+// Faire l'initialisation de tout les attributs
+CreatureExperience::CreatureExperience(const Creature& creature, unsigned int forceExp):Creature(creature), forceExperience_(forceExp){}
+//implementation apres ":"
+
 
 // todo:
 // Faire l'initialisation de tout les attributs
-CreatureExperience::CreatureExperience(const string& nom, unsigned int attaque, 
-		unsigned int defense, unsigned int pointDeVie, unsigned int energie, Pouvoir pouvoir, 
-		unsigned int forceExp)
-{
-    
-}
+CreatureExperience::CreatureExperience(const CreatureExperience& creature):Creature(creature) {}
+//implementation apres ":"
 
-// todo:
-// Faire l'initialisation de tout les attributs
-CreatureExperience::CreatureExperience(const Creature& creature, unsigned int forceExp)
-{
-    
-}
 
-// todo:
-// Faire l'initialisation de tout les attributs
-CreatureExperience:: CreatureExperience(const CreatureExperience & creature)
-{
-    
-}
 
 // todo
 // Faire la surcharge de l'opérateur = de maniere a ce que
@@ -36,6 +34,10 @@ CreatureExperience:: CreatureExperience(const CreatureExperience & creature)
 // dans l'objet courant.
 CreatureExperience& CreatureExperience::operator= (const CreatureExperience &  creature)
 {
+    if (this != &creature) {
+        Creature::operator=(creature);
+        forceExperience_ = creature.obtenirForceExperience();
+    }
 	   return *this;
 }
 
@@ -63,7 +65,58 @@ void CreatureExperience::attaquer(Creature& creature)
     // classe de base.
     
     // indice 2: Fiez-vous au test sur les attaques
-    
+
+    if (energie_ >= pouvoir_->obtenirEnergieNecessaire()) {
+        if (creature.obtenirPointDeVie() >= 0) {
+
+            int degat = 0;
+
+            int degatPotentiel = static_cast<int>(pouvoir_->obtenirNombreDeDegat()) * (static_cast<int>(attaque_) / 2);
+
+            // Determiner la defense restante de la creature
+            int defenseRestante = static_cast<int>(creature.obtenirDefense()) - degatPotentiel;
+            
+            if (defenseRestante < 0)
+            {
+                degat = -defenseRestante;
+                creature.modifierDefense(0);
+            }
+            else {
+                creature.modifierDefense(defenseRestante);
+            }
+
+            // L'attaque rate une fois sur 6
+            int tentative = rand() % 10;
+
+            if (tentative != 5) {
+
+                std::cout << nom_ << " lance " << pouvoir_->obtenirNom() << " qui inflige " << degat << " degat a " << creature.obtenirNom() << endl;
+
+                if (degat > creature.obtenirPointDeVie()) {
+                    creature.modifierPointDeVie(0);
+                    int xp = experienceGagnee(creature);
+
+                    cout << nom_ << " a gagne " << xp << " XP" << endl << endl;
+                }
+                else {
+                    creature.modifierPointDeVie(creature.obtenirPointDeVie() - degat);
+                }
+
+                cout << creature.obtenirNom() << " a encore " << creature.obtenirPointDeVie() << " PV" << endl;
+                energie_ -= pouvoir_->obtenirEnergieNecessaire();
+            }
+            else {
+                std::cout << "Attaque " << pouvoir_->obtenirNom() << " a echouee" << endl;
+            }
+        }
+        else {
+            std::cout << "Vous deja avez vaincu " << creature.obtenirNom() << endl;
+        }
+    }
+    else
+    {
+        cout << this->obtenirNom() << " n'as plus assez d'énergie pour se battre" << endl;
+    }
     
 }
 // todo
@@ -73,7 +126,7 @@ void CreatureExperience::attaquer(Creature& creature)
 // sa force experience.
 void CreatureExperience::afficher() const
 {
-
+    cout << "Creature Experience";
 }
 
 // todo
@@ -83,6 +136,9 @@ void CreatureExperience::afficher() const
 // de la classe parent.
 std::ostream& operator<<(std::ostream & os, const CreatureExperience& creature)
 {
+    creature.afficher();
+    os << static_cast<Creature>(creature);
+
 	return os;
 }
 
@@ -90,6 +146,7 @@ std::ostream& operator<<(std::ostream & os, const CreatureExperience& creature)
 // doit retourner l'attribut forceExperience_ de l'objet courant
 unsigned int CreatureExperience::obtenirExperience () const
 {
+
     return 0;
 }
 
