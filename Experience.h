3feb692865e0dac5273d5baa81e870bbb00bde66
SimonRiopel <98927317+SimonRiopel@users.noreply.github.com>
2022-02-24 16:17:31 -0500

    Update Experience.h

--- Experience.h
+++ Experience.h
@@ -3,7 +3,10 @@
 // à  faire : classe Interface
 class Experience
 {
-
-
+public:
+	virtual ~Experience() = default;
+	virtual void afficher() const = 0;
+	virtual unsigned int obtenirExperience() const = 0;
+	virtual string obtenirNomExperience() const = 0;
 };
 #endif
